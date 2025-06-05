9b3ee9d89f5f649448cc33c9c1af9cabd7b6e984
deep473 <deepztanshu@gmail.com>
2025-06-05 11:55:46 +0530

    sign up

--- com.salesSavvy/src/main/java/com/salesSavvy/controller/UsersController.java
+++ com.salesSavvy/src/main/java/com/salesSavvy/controller/UsersController.java
@@ -1,13 +1,17 @@
 package com.salesSavvy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.salesSavvy.entity.Users;
 import com.salesSavvy.service.UsersService;
 
+@CrossOrigin("*")
 @RestController
 public class UsersController {
 	@Autowired
